e7639c4cf933ce2faf23b1977b9896e5f40404db
Kitsuna31 <122208451+Kitsuna31@users.noreply.github.com>
2025-08-30 01:59:40 +0800

    Update SendKeyStrokes.ino

--- examples/SendKeyStrokes/SendKeyStrokes.ino
+++ examples/SendKeyStrokes/SendKeyStrokes.ino
@@ -28,17 +28,12 @@ void loop() {
 
     delay(1000);
 
-   //
-   // Below is an example of pressing multiple keyboard modifiers 
-   // which by default is commented out.
-    /*
     Serial.println("Sending Ctrl+Alt+Delete...");
     bleKeyboard.press(KEY_LEFT_CTRL);
     bleKeyboard.press(KEY_LEFT_ALT);
     bleKeyboard.press(KEY_DELETE);
     delay(100);
     bleKeyboard.releaseAll();
-    */
   }
 
   Serial.println("Waiting 5 seconds...");
